6a7811d8bb26ebd067d71e6b2b657f203aaa6c33
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-14 23:04:17 +0100

    Added MongoDB Connection

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+    users: [
+      {
+        name: "Kamsi",
+        email: "admin@example.com",
+        password: bcrypt.hashSync("1234", 8),
+        isAdmin: true
+      },
+      {
+        name: "Edoro",
+        email: "user@example.com",
+        password: bcrypt.hashSync("1234", 8),
+        isAdmin: false
+      }
+    ],
     products: [
       {
         _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+
+const dbURI = "mongodb+srv://kamsi:Password123@cluster0.9hlj4.mongodb.net/amazon?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => console.log("Connected DB!"));
 
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find(x => x._id === req.params.id);
@@ -17,9 +27,16 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
+// User Routes
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res) => {
     res.send("Server is ready")
 });
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+})
 
 app.listen(5000, () => {
     console.log("Serve at http://localhost:5000")
