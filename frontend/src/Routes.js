d3052f4d2d2fa56e31166c0f9606252cebbb30ef
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-15 00:03:13 +0100

    Added SignIn Screen

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom'
 import CartScreen from './screens/CartScreen'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import SigninScreen from './screens/SigninScreen'
 
 export default function Routes() {
     return (
         <div>
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/signin" component={SigninScreen} />
             <Route path="/" component={HomeScreen} exact />
         </div>
     )
