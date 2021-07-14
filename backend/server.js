eaaf738a86a94d2c72251a4bfdbed3ed7007301a
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-14 23:23:33 +0100

    Added Products Backend API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import data from "./data.js";
+import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 const app = express();
@@ -13,22 +14,25 @@ mongoose.connect(dbURI, {
     useCreateIndex: true
 }).then(() => console.log("Connected DB!"));
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find(x => x._id === req.params.id);
-    if (product) {
-        res.send(product);
-    } else {
-        res.status(404).send({ message: "Product not Found" });
-    }
+// app.get('/api/products/:id', (req, res) => {
+//     const product = data.products.find(x => x._id === req.params.id);
+//     if (product) {
+//         res.send(product);
+//     } else {
+//         res.status(404).send({ message: "Product not Found" });
+//     }
     
-});
+// });
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
+// app.get('/api/products', (req, res) => {
+//     res.send(data.products);
+// });
 
 // User Routes
 app.use('/api/users', userRouter);
+
+// Product Routes
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
     res.send("Server is ready")
