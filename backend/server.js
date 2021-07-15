5dfa7f4f3e63b1935521782c3ccf743c93c55a20
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-15 17:29:38 +0100

    Code Refactored For Paypal

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,18 @@ import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
+// Add CORS
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use(express.json()); // Parse Http Requests
 app.use(express.urlencoded({ extended: true }));
@@ -35,6 +43,10 @@ app.get('/api/config/paypal', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send("Server is ready")
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
 app.use((err, req, res, next) => {
