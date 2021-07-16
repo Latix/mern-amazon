f87e2f1913d2c0004d21faeb58c29c5c1dbbda77
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-16 21:39:21 +0100

    Code Fix

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect, Route } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import PageNotFound from './components/PageNotFound'
 import PrivateRoute from './components/PrivateRoute'
 import CartScreen from './screens/CartScreen'
@@ -15,16 +15,18 @@ import SigninScreen from './screens/SigninScreen'
 export default function Routes() {
     return (
         <div>
-            <Route exact path="/" component={HomeScreen} />
-            <Route exact path="/cart/:id?" component={CartScreen} />
-            <Route exact path="/product/:id" component={ProductScreen} />
-            <Route exact path="/register" component={RegisterScreen} />
-            <Route exact path="/signin" component={SigninScreen} />
-            <PrivateRoute exact path="/shipping" component={ShippingAddressScreen} />
-            <Route exact path="/payment" component={PaymentMethodScreen} />
-            <Route exact path="/placeorder" component={PlaceOrderScreen} />
-            <PrivateRoute exact path="/order/:id" component={OrderScreen} />
-            <Route component={PageNotFound} />
+            <Switch>
+                <Route exact path="/" component={HomeScreen} />
+                <Route exact path="/cart/:id?" component={CartScreen} />
+                <Route exact path="/product/:id" component={ProductScreen} />
+                <Route exact path="/register" component={RegisterScreen} />
+                <Route exact path="/signin" component={SigninScreen} />
+                <PrivateRoute exact path="/shipping" component={ShippingAddressScreen} />
+                <Route exact path="/payment" component={PaymentMethodScreen} />
+                <Route exact path="/placeorder" component={PlaceOrderScreen} />
+                <PrivateRoute exact path="/order/:id" component={OrderScreen} />
+                <Route component={PageNotFound} />
+            </Switch>
         </div>
     )
 }
